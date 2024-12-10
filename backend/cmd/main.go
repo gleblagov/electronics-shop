ff1395d9ee27a53f596103ba9cd0fb398fe709de
Gleb Lagov <devlagov@gmail.com>
2024-12-10 17:57:47 +0300

    feat(backend): basic user handlers

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 	"sync"
+
+	"github.com/gleblagov/electronics-shop/data"
+	"github.com/gleblagov/electronics-shop/handlers"
 )
 
 func main() {
+	ps, err := data.NewPostgresStorage()
+	if err != nil {
+		panic(err)
+	}
+
 	mux := http.NewServeMux()
 	slog.Info("starting server...")
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
+		mux.HandleFunc("GET /user/{id}", handlers.HandleUserGetById(context.TODO(), ps.Users))
+		mux.HandleFunc("POST /user", handlers.HandleUserNew(context.TODO(), ps.Users))
+		mux.HandleFunc("DELETE /user/{id}", handlers.HandleUserDelete(context.TODO(), ps.Users))
+
 		mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("pong"))
 			return
