2e47ae4e13d0b0638d1224d5646b39b03a77c369
Gleb Lagov <devlagov@gmail.com>
2025-01-08 00:31:38 +0300

    fix(migrations): поправил тип данных для даты

--- migrations/00004_sample_cart_table.sql
+++ migrations/00004_sample_cart_table.sql
@@ -4,8 +4,8 @@
 CREATE TABLE carts (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id),
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(15) NOT NULL DEFAULT 'created' CHECK (status IN ('created', 'closed', 'purchased'))
 );
 -- +goose StatementEnd
